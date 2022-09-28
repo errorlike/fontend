890b1d3daf6911b383361fa2c1181c214a14ae03
errorlike <errorlikeman@outlook.com>
2022-09-28 22:04:44 +0800

    deploy changes Changes to be committed: 	modified:   part2/phonebook/package.json 	modified:   part2/phonebook/src/server/persons.js

--- part2/phonebook/src/server/persons.js
+++ part2/phonebook/src/server/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons/";
 const getAll = () =>
     axios
         .get(baseUrl)
@@ -14,7 +14,6 @@ const deleteById = id =>
         .then(response => {
             console.log(response);
         });
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getAll,
     create,
