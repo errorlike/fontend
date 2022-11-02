c35858545dfac96ba95d795aa1549062365906d0
errorlike <errorlikeman@outlook.com>
2022-11-02 19:17:57 +0800

    exercise5.11-5.12

--- bloglist-frontend/src/components/LoginForm.js
+++ bloglist-frontend/src/components/LoginForm.js
@@ -1,30 +1,30 @@
-const LoginForm = ({username, password, handleLogin,  handleUsernameChange,  handlePasswordChange}) => {
-    return (
+const LoginForm = ({ username, password, handleLogin, handleUsernameChange, handlePasswordChange }) => {
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
         <div>
-            <form onSubmit={handleLogin}>
-                <div>
                     Username
-                    <input
-                        type='text'
-                        value={username}
-                        name='Username'
-                        onChange={handleUsernameChange}
-                    />
-                </div>
-                <div>
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
                     Password
-                    <input
-                        type='password'
-                        value={password}
-                        name='Password'
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type='submit'>
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type='submit'>
                     login
-                </button>
-            </form>
-        </div>);
-};
+        </button>
+      </form>
+    </div>)
+}
 
-export default LoginForm;
+export default LoginForm
--- bloglist-frontend/src/components/Togglable.js
+++ bloglist-frontend/src/components/Togglable.js
@@ -1,27 +1,32 @@
-import { forwardRef, useImperativeHandle, useState } from "react";
+import { forwardRef, useImperativeHandle, useState } from 'react'
+import propTypes from 'prop-types'
+// eslint-disable-next-line react/display-name
 const Togglable = forwardRef(({ buttonLabel, children }, ref) => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false)
 
-    const hideWhenVisible = { display: visible ? 'none' : null };
-    const showWhenVisible = { display: visible ? null : 'none' };
+  const hideWhenVisible = { display: visible ? 'none' : null }
+  const showWhenVisible = { display: visible ? null : 'none' }
 
-    const toggleVisibility = () => {
-        setVisible(!visible);
-    };
-    useImperativeHandle(ref, () => {
-        return { toggleVisibility };
-    });
-    return (
-        <div>
-            <div style={hideWhenVisible}>
-                <button onClick={toggleVisibility}>{buttonLabel}</button>
-            </div>
-            <div style={showWhenVisible}>
-                {children}
-                <button onClick={toggleVisibility}>cancel</button>
-            </div>
-        </div>);
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  useImperativeHandle(ref, () => {
+    return { toggleVisibility }
+  })
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>)
 
-});
+})
 
-export default Togglable;
+Togglable.propTypes = {
+  buttonLabel: propTypes.string.isRequired
+}
+export default Togglable
