553fed2e8f037ea6c83c73c48e2eab5f55bff59f
errorlike <errorlikeman@outlook.com>
2022-11-05 17:29:27 +0800

    exercise 5.16

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import Blog from './Blog';
 import '@testing-library/jest-dom/extend-expect';
-import { getByText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 const blog = {
