7812dcd6abf62fa33aced34920af5538fab282d9
errorlike <errorlikeman@outlook.com>
2022-11-06 23:17:49 +0800

    exercise 6.1

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -2,22 +2,37 @@ const initialState = {
   good: 0,
   ok: 0,
   bad: 0
-}
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
+    case 'GOOD': {
+      const changedState = {
+        ...state,
+        good: state.good + 1
+      };
+      return changedState;
+    }
+    case 'OK': {
+      const changedState = {
+        ...state,
+        ok: state.ok + 1
+      };
+      return changedState;
+    }
+    case 'BAD': {
+      const changedState = {
+        ...state,
+        bad: state.bad + 1
+      };
+      return changedState;
+    }
     case 'ZERO':
-      return state
-    default: return state
+      return initialState;
+    default: return state;
   }
-  
-}
 
-export default counterReducer
+};
+
+export default counterReducer;
